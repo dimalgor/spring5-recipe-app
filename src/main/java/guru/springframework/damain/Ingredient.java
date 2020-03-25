e8baf1dcef83d700431cd10feec52c09100d1cb3
Dmytro Horban <dmytro.horban@gmail.com>
2020-03-25 16:13:21 +0200

    Added basic entities.

---
+++ src/main/java/guru/springframework/damain/Ingredient.java
@@ -0,0 +1,59 @@
+package guru.springframework.damain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uof;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUof() {
+        return uof;
+    }
+
+    public void setUof(UnitOfMeasure uof) {
+        this.uof = uof;
+    }
+}
